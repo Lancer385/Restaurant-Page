492c4c9e4fc403f210114d167ed190e0e9f12d48
lancer385 <ahmedkhalaf1234123@gmail.com>
2024-08-16 14:57:16 +0300

    add fucntionality to remove all child nodes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-console.log("Hello World!");
-condole.log("he");
+import { swtichTab } from "./main-page";
+import "./Style.css";
+const container = document.querySelector("#content");
+export {container}
+swtichTab();
+const home = document.querySelector("#home");
+home.addEventListener('click', () => {
+    removeAllChilds(container);
+});
+
+function removeAllChilds(parent){
+    while (parent.firstChild){
+        parent.removeChild(container.lastChild);
+    };
+};
--- src/main-page.js
+++ src/main-page.js
@@ -1,3 +1,20 @@
-export function displayPage(){
+import {container} from "./index.js";
+
+export function swtichTab(){
+
+    for (let child of main){
+        container.appendChild(child);
+    }
 
 };
+
+const main = [
+    document.createElement("h1"),
+    document.createElement("img"),
+    document.createElement("div"),
+    document.createElement("p")
+];
+
+main[0].textContent = "Welcome to Nostalgic Bites";
+main[2].textContent = "just whatever";
+const div = document.createElement("div");
