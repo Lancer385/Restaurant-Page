d65145e7d2d28e51cba0bbe847c65a2fc3bb171b
lancer385 <ahmedkhalaf1234123@gmail.com>
2024-08-17 18:32:21 +0300

    add menu DOM elements for dishes

--- src/menu-page.js
+++ src/menu-page.js
@@ -1,21 +1,40 @@
 import { container } from "./index";
+import img1 from "./images/chicken-wrap.jpg";
+import img2 from "./images/Stuffed-peppers.jpg";
+import img3 from "./images/brooke-lark-unsplash.jpg";
 export function addMenu(){
-    for (const child of menu){
-        container.appendChild(child);
-    }
+    container.appendChild(menu.title);
+    container.appendChild(menu.dishes);
 }
-const menu = [
-    document.createElement("h1"),
-    document.createElement("div"),
-]
-const images = [
-    document.createElement("img"),
-    document.createElement("img"),
-    document.createElement("img"),
-    document.createElement("img")
-];
+const menu = {
+    title : document.createElement("h1"),
+    dishes: document.createElement("div"),
+}
+menu.title.textContent = "OUR FINEST DISHES";
+menu.dishes.classList = "menu";
 
-menu[0].textContent = "OUR FINEST DISHES"
-for (let img of images){
-    menu[1].appendChild(img)
-}
+
+const imgSource = [img1, img2, img3]
+const dishImages = imgSource.map(src => createImage(src,"dish", ''));
+const textDesc = ["blah blah","bla","blah"];
+const dishDesc = textDesc.map(textContent => createText(textContent, "description"));
+for (let i = 0; i < dishImages.length; i++){
+    menu.dishes.appendChild(dishImages[i])
+    menu.dishes.appendChild(dishDesc[i])
+};
+
+
+export function createImage(src, className, alt = ''){
+    const img = document.createElement("img");
+    img.src = src;
+    img.alt = alt;
+    img.className = className;
+    return img;
+};
+
+function createText(textContent, className){
+    const text = document.createElement("p");
+    text.textContent = textContent;
+    text.className = className;
+    return text;
+};
