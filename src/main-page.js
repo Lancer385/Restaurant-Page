ca7e535cce160289fdfb8f1f2a9f545832443718
lancer385 <ahmedkhalaf1234123@gmail.com>
2024-08-17 19:29:10 +0300

    update some text

--- src/main-page.js
+++ src/main-page.js
@@ -1,4 +1,6 @@
 import {container} from "./index.js";
+import picture from "./images/patrick-schneider-unsplash.jpg";
+import { createImage } from "./menu-page.js";
 
 export function addMain(){
     for (let child of main){
@@ -8,11 +10,10 @@ export function addMain(){
 
 const main = [
     document.createElement("h1"),
-    document.createElement("img"),
-    document.createElement("div"),
+    createImage(picture, ''),
     document.createElement("p")
 ];
 
 main[0].textContent = "Welcome to Nostalgic Bites";
-main[2].textContent = "just whatever";
-const div = document.createElement("div");
+main[1].className = "mainImg";
+main[2].textContent = "Where you get taste of 70s, 80s, and 90s eras. Have a Classic favorites? maybe wanna feel those vintage vibes? Come and find out!";
